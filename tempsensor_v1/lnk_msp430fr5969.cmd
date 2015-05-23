a52987fa61f995a307e2e6fb8caba68aaf6a48b5
sergioamr <sergio.martinez.rodriguez@intel.com>
2015-05-23 15:28:56 +0100

    IMEI reworking
    Checking that the IMEI is the right one

--- tempsensor_v1/lnk_msp430fr5969.cmd
+++ tempsensor_v1/lnk_msp430fr5969.cmd
@@ -174,7 +174,7 @@ SECTIONS
     	i2c.obj (.aggregate_vars)
     	uart.obj (.aggregate_vars)
     	globals.obj (.xbigdata_vars)
-    } > FRAM
+    } >> FRAM
     .stack      : {} > RAM (HIGH)           /* SOFTWARE SYSTEM STACK             */
 
     .infoA     : {} > INFOA
