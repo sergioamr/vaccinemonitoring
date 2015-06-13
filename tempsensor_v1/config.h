414c337bce169d6e6c907e3e898e54815ba46949
danielcattlin <danielcattlin@hotmail.co.uk>
2015-06-13 20:48:21 +0100

    Reducing ATresp size

--- tempsensor_v1/config.h
+++ tempsensor_v1/config.h
@@ -98,9 +98,9 @@ EXTERN int8_t g_iSystemSetup;
 
 //Sampling configuration
 #define SAMPLE_PERIOD			1		//in minutes
-#define UPLOAD_PERIOD			10		//in minutes
-#define SMS_RX_POLL_INTERVAL	5		//poll interval in minutes for sms msg TODO change back
-#define LCD_REFRESH_INTERVAL	1
+#define UPLOAD_PERIOD			2		//in minutes
+#define SMS_RX_POLL_INTERVAL	3		//poll interval in minutes for sms msg TODO change back
+#define LCD_REFRESH_INTERVAL	5
 #define MSG_REFRESH_INTERVAL	5
 #define SAMPLE_COUNT			100
 
