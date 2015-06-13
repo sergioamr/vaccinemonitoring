f1f088bc5b8723e9bcdd4c358c725670aa5fd67a
sergioamr <sergio.martinez.rodriguez@intel.com>
2015-06-13 21:19:54 +0100

    Reduce a RX buffer to allocate Dan's code in memory

--- tempsensor_v1/modem_uart.h
+++ tempsensor_v1/modem_uart.h
@@ -8,8 +8,8 @@
 #ifndef TEMPSENSOR_V1_UART_H_
 #define TEMPSENSOR_V1_UART_H_
 
-#define TX_LEN   			1024
-#define RX_LEN   			2048
+#define TX_LEN   			640
+#define RX_LEN   			1024
 #define TOKEN_LEN			3
 
 #define ATCMD_HTTPSND		1
