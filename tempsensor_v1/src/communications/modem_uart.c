7c90a2c40f6cdff66597e65560c37083946b8fad
sergioamr <sergio.martinez.rodriguez@intel.com>
2015-06-25 12:00:08 +0100

    Saving extra modem info

--- tempsensor_v1/src/communications/modem_uart.c
+++ tempsensor_v1/src/communications/modem_uart.c
@@ -197,7 +197,9 @@ void sendCommand(const char *cmd) {
 	uart.iTxLen = strlen(cmd);
 
 	if (g_pDevCfg->cfg.logs.modem_transactions) {
+		log_modem("-------- ");
 		log_modem(get_simplified_date_string(NULL));
+		log_modem("-------- \r\n");
 		log_modem(cmd);
 	}
 
