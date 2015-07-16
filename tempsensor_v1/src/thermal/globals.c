ddc3d0f295be16d6879789f18f230c5f5e8ebb3c
sergioamr <sergio.martinez.rodriguez@intel.com>
2015-07-16 16:44:26 +0100

    Fixing some conflict

--- tempsensor_v1/src/thermal/globals.c
+++ tempsensor_v1/src/thermal/globals.c
@@ -34,6 +34,7 @@ volatile uint8_t g_iDisplayId = 0;
 #pragma SET_DATA_SECTION(".helper_vars")
 //put all variables that are written less frequently
 struct tm g_tmCurrTime;
+struct tm g_lastSampleTime;
 
 time_t g_timeNextPull;  // Pulltime from modem every 12 hours
 #pragma SET_DATA_SECTION()
